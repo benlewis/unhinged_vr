70d6c7f319aa65686ca46587c73aefb768cb0600
Ben Lewis <ben.lewis@gmail.com>
2014-09-17 11:42:12 -0700

    room with prize

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+	public float distPerSecond = 0.2f;
+	public float maxDistance = 1.0f;
+	
+	private float distanceMoved = 0.0f;
+	private bool moving = false;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1") && distanceMoved < maxDistance) {
+			Debug.Log ("Fire!");
+			moving = true;
+		}
+		
+		if (moving) {
+			float deltay = distPerSecond * Time.deltaTime;
+			distanceMoved += deltay;
+			transform.Translate(0, deltay, 0);
+			
+			if (distanceMoved >= maxDistance)
+				moving = false;
+		}
+	}
+}
