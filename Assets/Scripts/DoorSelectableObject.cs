152539680ec67908729eb4b6f661b0395e3b486d
Ben Lewis <benlewis@fb.com>
2014-10-11 17:19:57 -0700

    open door with a hinge

---
+++ Assets/Scripts/DoorSelectableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSelectableObject : SelectableObject {
+
+	private bool open = false;
+
+	
+	public override void Select() {
+		open = true;
+		hingeJoint.useMotor = true;
+		SetSelectable(false);
+	}
+	
+	public override bool IsSelectable() {
+		return (!open);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
