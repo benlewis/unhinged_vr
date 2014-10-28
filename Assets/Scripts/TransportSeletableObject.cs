3c41edc50582ef190cb7b8b2ac94af66fca83048
Ben Lewis <ben.lewis@gmail.com>
2014-10-27 23:15:26 -0700

    added transport platform

---
+++ Assets/Scripts/TransportSeletableObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransportSeletableObject : SelectableObject {
+	
+	public Transform pathToFollow;
+	public string levelName;
+	
+	private Color baseColor;
+	
+	public void Start() {
+		baseColor = renderer.material.color;
+	}
+
+	public override void Select() {
+
+	}
+	
+	public override void TurnOnHighlight() {
+		base.TurnOnHighlight();
+		
+		Debug.Log ("turn on highlight");
+		renderer.material.color = Color.green;
+	}
+	
+	public override void TurnOffHighlight() {
+		base.TurnOffHighlight();
+		
+		renderer.material.color = baseColor;
+	}
+}
