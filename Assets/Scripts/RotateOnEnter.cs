459d36a05289dde2ba666f8e99d749465021b610
Ben Lewis <ben.lewis@gmail.com>
2014-10-29 15:46:14 -0700

    level selector working

---
+++ Assets/Scripts/RotateOnEnter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateOnEnter : MonoBehaviour {
+
+	public float rotateSpeed = 2.0f;
+	private bool rotate = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (rotate)
+			transform.Rotate (0, rotateSpeed, 0);
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		rotate = true;
+	}
+}
