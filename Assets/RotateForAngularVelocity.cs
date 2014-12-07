2a5d7af0d4fa9e8a6d35049803d729623eaa808a
Ben Lewis <benlewis@fb.com>
2014-12-06 21:53:41 -0800

    make gear prefabs rotateable in 3d space

--- Assets/RotateForAngularVelocity.cs
+++ Assets/RotateForAngularVelocity.cs
@@ -5,15 +5,24 @@ public class RotateForAngularVelocity : MonoBehaviour {
 
 	public Vector3 velocityToAchieve = Vector3.zero;
 
+	private Vector3 zLock;
+
 	// Use this for initialization
 	void Start () {
-	
+		zLock = transform.localEulerAngles;
+		Debug.Log ("Starts at " + zLock);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (rigidbody.angularVelocity != velocityToAchieve) {
-			rigidbody.AddTorque(velocityToAchieve - rigidbody.angularVelocity);
+	void FixedUpdate () {
+		Vector3 localangularvelocity = transform.InverseTransformDirection(rigidbody.angularVelocity);
+		if (localangularvelocity != velocityToAchieve) {
+			rigidbody.AddRelativeTorque(velocityToAchieve - localangularvelocity);
 		}
+	}
+	
+	void LateUpdate() {
+		zLock.z = transform.localEulerAngles.z;
+		transform.localEulerAngles = zLock;
 	}
 }
